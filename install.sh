371baad4a3e37e2e47b21b16c36f317a0770cb9a
0sifat <ssifat207@gmail.com>
2024-03-15 15:34:46 +0600

    2nd commit

--- install.sh
+++ install.sh
@@ -7,4 +7,9 @@ echo "Create a Directory Structure"
 mkdir /var/www/html/site1.example.com
 mkdir /var/www/html/site2.example.com
 
-/var/www/html/site1.example.com/index.html
+
+echo "<html>
+<title>site1.example.com</title>
+<h1>Welcome to site1.example.com Website</h1>
+<p>This is my first website hosted with name-based virtual hosting</p>
+</html> " /var/www/html/site1.example.com/index.html
