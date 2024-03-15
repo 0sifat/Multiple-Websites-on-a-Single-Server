c76c1ddfe49edb3ecfe17d1cec0d79ddf48c9eeb
0sifat <ssifat207@gmail.com>
2024-03-15 15:45:17 +0600

    secound Commint

--- install.sh
+++ install.sh
@@ -12,16 +12,23 @@ echo "<html>
 <title>site1.example.com</title>
 <h1>Welcome to site1.example.com Website</h1>
 <p>This is my first website hosted with name-based virtual hosting</p>
-</html> " /var/www/html/site1.example.com/index.html
+</html> " > /var/www/html/site1.example.com/index.html
 
 echo "<html>
 <title>site2.example.com</title>
 <h1>Welcome to site2.example.com Website</h1>
 <p>This is my first website hosted with name-based virtual hosting</p>
-</html> " /var/www/html/site2.example.com/index.html
+</html> " > /var/www/html/site2.example.com/index.html
 
 
 chown -R www-data:www-data /var/www/html/site1.example.com
 chown -R www-data:www-data /var/www/html/site2.example.com
 
-
+echo "<VirtualHost *:80>
+ServerAdmin admin@site1.example.com
+ServerName site1.example.com
+DocumentRoot /var/www/html/site1.example.com
+DirectoryIndex index.html
+ErrorLog ${APACHE_LOG_DIR}/site1.example.com_error.log
+CustomLog ${APACHE_LOG_DIR}/site1.example.com_access.log combined
+</VirtualHost>" > /etc/apache2/sites-available/site1.example.com.conf
